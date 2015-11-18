264232adf9add175bcf721160eb1d2969ca1f979
inikulin <ifaaan@gmail.com>
2015-11-18 13:24:10 +0300

    Debug only tasks, refactoring, new API

--- index.js
+++ index.js
@@ -3,71 +3,105 @@ var gulp        = require('gulp');
 var PluginError = require('gulp-util').PluginError;
 var Promise     = require('pinkie-promise');
 
-var isWorker = process.argv.indexOf('--ll-worker') > -1;
-var disabled = process.argv.indexOf('--no-ll') > -1;
+function getTaskListFromArgs (args) {
+    if (Array.isArray(args[0]))
+        return args[0];
 
-var args = process.argv.slice().filter(function (arg) {
-    // NOTE: remove debugger breakpoints from worker args
-    return arg.indexOf('--debug-brk') < 0;
-});
-
-var nodePath     = args.shift();
-var tasks        = [];
-var originalTask = gulp.task;
-
-function getSpawnArgs (taskName) {
-    var spawnArgs = args.filter(function (arg) {
-        return tasks.indexOf(arg) < 0;
-    });
-
-    spawnArgs.push('ll:' + taskName);
-    spawnArgs.push('--ll-worker');
-
-    return spawnArgs;
+    return Array.prototype.slice.call(args);
 }
 
-function createLLTask (name) {
-    return new Promise(function (resolve, reject) {
-        var llTask = spawn(nodePath, getSpawnArgs(name), { stdio: 'inherit' });
+function GulpLL () {
+    this.taskFn = gulp.task.bind(gulp);
 
-        llTask.on('exit', function (code) {
+    this.llTasks          = [];
+    this.llTasksDebugOnly = [];
+    this.allTasks         = [];
+
+    this.isDebug  = typeof v8debug !== 'undefined' || process.argv.indexOf('--ll-debug') > -1;
+    this.isWorker = process.argv.indexOf('--ll-worker') > -1;
+
+    this.args = process.argv.slice(1).filter(function (arg) {
+        // NOTE: remove debugger breakpoints from worker args
+        return arg.indexOf('--debug-brk') < 0;
+    });
+
+    if (process.argv.indexOf('--no-ll') < 0)
+        this._overrideGulpTaskFn()
+}
+
+
+GulpLL.prototype._getWorkerArgs = function (task) {
+    var ll = this;
+
+    var args = this.args.filter(function (arg) {
+        return ll.allTasks.indexOf(arg) < 0;
+    });
+
+    args.push('worker:' + task);
+    args.push('--ll-worker');
+
+    if (this.isDebug)
+        args.push('--ll-debug');
+
+    return args;
+};
+
+GulpLL.prototype._createWorker = function (task) {
+    var worker = spawn(process.execPath, this._getWorkerArgs(task), { stdio: 'inherit' });
+
+    return new Promise(function (resolve, reject) {
+        worker.on('exit', function (code) {
             if (code === 0)
                 resolve();
             else {
                 reject(new PluginError('ll', {
-                    message: 'Task ll:' + name + ' failed'
+                    message: 'Task ll:' + task + ' failed'
                 }));
             }
         })
     });
-}
-
-gulp.task = function (name, deps, fn) {
-    // NOTE: collect task names, so we
-    // can filter them from worker args
-    tasks.push(name);
-
-    originalTask.call(gulp, name, deps, fn);
 };
 
-gulp.llTask = function (name, deps, fn) {
-    if (!fn && typeof deps === 'function') {
-        fn   = deps;
-        deps = void 0;
-    }
-
-    if (disabled)
-        gulp.task(name, deps, fn);
-
-    else if (isWorker) {
-        // NOTE: we should run without deps, since
-        // they are already executed in the master
-        gulp.task('ll:' + name, fn);
-    }
-
-    else {
-        gulp.task(name, deps, function () {
-            return createLLTask(name);
-        });
-    }
+GulpLL.prototype._isLLTask = function (task) {
+    return this.llTasks.indexOf(task) > -1 || (this.isDebug && this.llTasksDebugOnly.indexOf(task) > -1);
 };
+
+GulpLL.prototype._overrideGulpTaskFn = function () {
+    var ll = this;
+
+    gulp.task = function (name, deps, fn) {
+        if (!fn && typeof deps === 'function') {
+            fn   = deps;
+            deps = void 0;
+        }
+
+        if (ll._isLLTask(name)) {
+            if (ll.isWorker) {
+                deps = void 0;
+                name = 'worker:' + name;
+            }
+            else {
+                fn = function () {
+                    return ll._createWorker(name);
+                };
+            }
+        }
+
+
+        ll.allTasks.push(name);
+        ll.taskFn(name, deps, fn);
+    };
+};
+
+GulpLL.prototype.tasks = function () {
+    this.llTasks = getTaskListFromArgs(arguments);
+    return this;
+};
+
+GulpLL.prototype.onlyInDebug = function () {
+    this.llTasksDebugOnly = getTaskListFromArgs(arguments);
+    return this;
+};
+
+
+module.exports = new GulpLL();
